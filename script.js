984cd49571f1624b3f47d522ee8ef2c88072bdbe
navneet-chugh <navneetchugh15@gmail.com>
2023-08-21 21:47:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function getBrowserInfo() {
+  const userAgent = navigator.userAgent;
+  const browserName = navigator.appName;
+  const browserVersion = navigator.appVersion;
+
+  const message = "You are using " + browserName + " version " + browserVersion;
+  const browserInfoElement = document.getElementById('browser-info');
+  browserInfoElement.textContent = message;
+}
+
+getBrowserInfo();
